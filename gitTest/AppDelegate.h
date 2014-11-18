29f06d2fbd697425792ad3bef3d32422ae5ca359
James Stiehl <jstiehl@gs-mbp15-james.local>
2014-11-17 20:24:28 -0800

    my first commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by James Stiehl on 11/17/14.
+//  Copyright (c) 2014 James Stiehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by James Stiehl on 11/17/14.
+//  Copyright (c) 2014 James Stiehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
